fb54509b2cac6c498e2265a61849ff9fdc0a36e6
Renan Roger <eynanx@gmail.com>
2021-04-04 20:06:16 -0300

    feat: create model repository

--- scan/models.py
+++ scan/models.py
@@ -1,21 +1,30 @@
 from django.db import models
 
+MAXIMUM_VARCHAR_SIZE_MYSQL = 191  # becase of utf8mb4
+
+
 class User(models.Model):
     id = models.IntegerField(primary_key=True, null=False)
-    login = models.CharField(max_length=250, null=False, blank=False)
-    url = models.CharField(max_length=250, null=False, blank=False)
-    name = models.CharField(max_length=250, null=True, blank=True)
-    company = models.CharField(max_length=250, null=True, blank=True)
-    location = models.CharField(max_length=250, null=True, blank=True)
-    email = models.CharField(max_length=250, null=True, blank=True)
-    bio = models.TextField(null=True, blank=True)
-    twitter_username = models.CharField(max_length=250, null=True, blank=True)
-    public_repos = models.IntegerField(null=False)
-    public_gists = models.IntegerField(null=False)
-    followers = models.IntegerField(null=False)
-    following = models.IntegerField(null=False)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    login = models.CharField(max_length=MAXIMUM_VARCHAR_SIZE_MYSQL, null=False, blank=False)
+    url = models.CharField(max_length=MAXIMUM_VARCHAR_SIZE_MYSQL, null=False, blank=False)
 
     def __str__(self):
         return self.login
+
+
+class Repository(models.Model):
+    id = models.IntegerField(primary_key=True, null=False)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    description = models.TextField(null=True, blank=True)
+    name = models.CharField(max_length=MAXIMUM_VARCHAR_SIZE_MYSQL, null=False, blank=False)
+    full_name = models.CharField(max_length=MAXIMUM_VARCHAR_SIZE_MYSQL, null=False, blank=False)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    pushed_at = models.DateTimeField()
+    language = models.CharField(max_length=MAXIMUM_VARCHAR_SIZE_MYSQL, null=True, blank=True)
+    forks_count = models.IntegerField(null=False)
+    stargazers_count = models.IntegerField(null=False)
+    watchers_count = models.IntegerField(null=False)
+
+    def __str__(self):
+        return self.name

c981d4031f884cb684866fe3d36e3c0e5794fce6
Renan Roger <eynanx@gmail.com>
2021-04-10 17:39:51 -0300

    Revert "test: Create user gateways tests"
    This reverts commit 25365e71

--- scan/gateways/repository_gateway.py
+++
@@ -1,6 +0,0 @@
-from scan.models import Repository
-
-
-def create_repositories_in_lot(repositories):
-    repositories = [Repository(**repository) for repository in repositories]
-    Repository.objects.bulk_create(repositories)

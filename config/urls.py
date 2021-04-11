d06fdaf026091f808f82ebec2c75450868a622e0
Renan Roger <eynanx@gmail.com>
2021-04-11 13:03:51 -0300

    test: create test for serializers.

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 
 from scan.views import UserApiView, RepositoryApiView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('users', UserApiView.as_view()),
     path('repositories', RepositoryApiView.as_view())
 ]
---
+++ scan/tests/test_serializers.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+
+from scan.models import User, Repository
+from scan.serializers import UserSerializer, RespositorySerializer
+from scan.tests.utils import get_repository_struct
+
+
+class UserSerializerTests(TestCase):
+    def test_contains_expected_fields(self):
+        user = User.objects.create(id=1, login='user', url='http://teste.com')
+        serializer = UserSerializer(instance=user)
+
+        data = serializer.data
+
+        self.assertEqual({f.name for f in User._meta.fields}, set(data.keys()))
+
+
+class RepositorySerializerTests(TestCase):
+    def test_contains_expected_fields(self):
+        user = User.objects.create(id=1, login='user', url='http://teste.com',)
+        repository = Repository.objects.create(**get_repository_struct(id=1, user_id=1))
+        serializer = RespositorySerializer(instance=repository)
+
+        data = serializer.data
+
+        self.assertEqual({f.name for f in Repository._meta.fields}, set(data.keys()))

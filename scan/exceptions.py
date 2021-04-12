bfd8a82e5976012cc57f690225318e0afc734190
Renan Roger <eynanx@gmail.com>
2021-04-11 21:43:17 -0300

    test: alter exception.

--- scan/exceptions.py
+++ scan/exceptions.py
@@ -1,5 +1,5 @@
 
 class GithubException(Exception):
-    def __init__(self, code: int, reason: str):
-        message = f'Github api returned the status code {code}, reason: {reason}.'
+    def __init__(self, code: int, reason: str, detail: str):
+        message = f'Github api returned the status code {code}, reason: {reason}, detail: {detail}.'
         super(GithubException, self).__init__(message)
--- scan/tests/test_exceptions.py
+++ scan/tests/test_exceptions.py
@@ -6,8 +6,8 @@ from scan.exceptions import GithubException
 class GithubExceptionTests(TestCase):
     def test_return_formatted_message(self):
         def fn():
-            raise GithubException(500, 'Bad request')
+            raise GithubException(code=500, reason='Bad request', detail='wrong login')
 
         with self.assertRaises(GithubException) as context:
             fn()
-        self.assertEqual('Github api returned the status code 500, reason: Bad request.', str(context.exception))
+        self.assertEqual('Github api returned the status code 500, reason: Bad request, detail: wrong login.', str(context.exception))
